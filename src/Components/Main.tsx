37af4a89d72edf66438b2ced8e237559e5b6ef7f
TylerS2000 <tylerschulten@gmail.com>
2023-02-13 19:05:21 -0500

    fixed state issue

--- src/Components/Main.tsx
+++ src/Components/Main.tsx
@@ -3,29 +3,41 @@ import Row from "./Row";
 import React from "react";
 export default function Main() {
     const [answer, setAnswer] = React.useState("");
-    
-    
+    const [row, setRow] = React.useState(0);
+    const [answers, setAnswers] = React.useState(["","","","","",""]);
+    const [correctAnswer, setCorrectAnswer] = React.useState("Mario");
         onkeydown = (e) => {
             if (answer.length<5&&e.key.match(/[a-zA-Z]/)&&e.key.length<2) {
-            setAnswer((prev)=>prev+e.key);
-
+                setAnswer((prev)=>{return prev+e.key});
             }
             if (e.key === "Backspace") {
                 setAnswer((prev)=>{return prev.substring(0, prev.length-1)});
             }
-        }
-          console.log(answer);
+            if (e.key === "Enter"&&answer.length===5) {
+                setAnswer("");
+                setRow((prev)=>prev+1);
+            }
+            console.log(e.key);
             
-    
+        }
+          console.log(answers,row,answer);
+            
+    React.useEffect(() => {
+        setAnswers((prev)=>{
+            let newAnswers = [...prev];
+            newAnswers[row] = answer;
+            return newAnswers;
+        });
+    }, [answer,row]);
+    //useEffect causes answer array to update with input
     return (
         <div className="AnswerGrid">
-            <Row answer={answer}/>
-            <Row answer={answer}/>
-            <Row answer={answer}/>
-            <Row answer={answer}/>
-            <Row answer={answer}/>
-            <Row answer={answer}/>
-            <input/>
+            <Row answer={answers[0]} row={0}/>
+            <Row answer={answers[1]} row={1}/>
+            <Row answer={answers[2]} row={2}/>
+            <Row answer={answers[3]} row={3}/>
+            <Row answer={answers[4]} row={4}/>
+            <Row answer={answers[5]} row={5}/>
         </div>
     );
     }
--- src/Components/Row.tsx
+++ src/Components/Row.tsx
@@ -1,11 +1,11 @@
-export default function Row(props: {answer: string}) {
+export default function Row(props: {answer: string, row: number}) {
     return (
         <div className="row">
-            <div className="AnswerBox">{props.answer[0]}</div>
-            <div className="AnswerBox">{props.answer[1]}</div>
-            <div className="AnswerBox">{props.answer[2]}</div>
-            <div className="AnswerBox">{props.answer[3]}</div>
-            <div className="AnswerBox">{props.answer[4]}</div>
+            <div className="AnswerBox">{props.answer[0]||""}</div>
+            <div className="AnswerBox">{props.answer[1]||""}</div>
+            <div className="AnswerBox">{props.answer[2]||""}</div>
+            <div className="AnswerBox">{props.answer[3]||""}</div>
+            <div className="AnswerBox">{props.answer[4]||""}</div>
         </div>
     );
 }
