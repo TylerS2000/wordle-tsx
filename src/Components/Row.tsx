a46e6e4064b4153226fdc1480953ad9515579776
Tyler <schultty@mail.uc.edu>
2023-02-23 22:44:40 -0500

    color fixes

--- src/Components/Row.tsx
+++ src/Components/Row.tsx
@@ -2,7 +2,9 @@ import React from 'react';
 export default function Row(props: {answer: string, row: number, current: number, correctAnswer: string, win: boolean, setWin: Function}) {
     let [checked,setChecked] = React.useState(false);
     let [color ,setColor] = React.useState(["#ffffff","#ffffff","#ffffff","#ffffff","#ffffff"]);
+    let [textColor, setTextColor] = React.useState("#000000");
    if(props.row===props.current-1&&checked===false){setChecked(true); checkAnswer(); console.log();
+    setTextColor("#ffffff");
    }
 
    function checkAnswer(){
@@ -10,8 +12,8 @@ export default function Row(props: {answer: string, row: number, current: number
             console.log(props.correctAnswer);
             
             if(props.answer[i]===props.correctAnswer[i]){setColor((prev)=>{let newColor = [...prev]; newColor[i] = "#6aaa64"; return newColor;});}
-            else if(props.correctAnswer.includes(props.answer[i])){setColor((prev)=>{let newColor = [...prev]; newColor[i] = "#ffff00"; return newColor;});}
-            else{setColor((prev)=>{let newColor = [...prev]; newColor[i] = "#ff0000"; return newColor;});}
+            else if(props.correctAnswer.includes(props.answer[i])){setColor((prev)=>{let newColor = [...prev]; newColor[i] = "#c9b458"; return newColor;});}
+            else{setColor((prev)=>{let newColor = [...prev]; newColor[i] = "#787c7e"; return newColor;});}
          }
          console.log(JSON.stringify(color),JSON.stringify(["#00ff00","#00ff00","#00ff00","#00ff00","#00ff00"]));
          
@@ -21,11 +23,11 @@ export default function Row(props: {answer: string, row: number, current: number
     }
     return (
         <div className="row">
-            <div className="AnswerBox" style={{"backgroundColor":color[0]}}>{props.answer[0]||""}</div>
-            <div className="AnswerBox" style={{"backgroundColor":color[1]}}>{props.answer[1]||""}</div>
-            <div className="AnswerBox" style={{"backgroundColor":color[2]}}>{props.answer[2]||""}</div>
-            <div className="AnswerBox" style={{"backgroundColor":color[3]}}>{props.answer[3]||""}</div>
-            <div className="AnswerBox" style={{"backgroundColor":color[4]}}>{props.answer[4]||""}</div>
+            <div className="AnswerBox" style={{"backgroundColor":color[0], "color":textColor}}>{props.answer[0]||""}</div>
+            <div className="AnswerBox" style={{"backgroundColor":color[1], "color":textColor}}>{props.answer[1]||""}</div>
+            <div className="AnswerBox" style={{"backgroundColor":color[2], "color":textColor}}>{props.answer[2]||""}</div>
+            <div className="AnswerBox" style={{"backgroundColor":color[3], "color":textColor}}>{props.answer[3]||""}</div>
+            <div className="AnswerBox" style={{"backgroundColor":color[4], "color":textColor}}>{props.answer[4]||""}</div>
         </div>
     );
 }
