908ae91a150d1204df9f1acae4f378bf467836c6
Tyler Schulten <tylerschulten@gmail.com>
2023-03-04 09:42:57 -0500

    formatting

--- src/Components/WinPopup.tsx
+++ src/Components/WinPopup.tsx
@@ -1,7 +1,9 @@
 import "./WinPopup.css"
 export default function WinPopup(props:{win:boolean, answer:string}){
     return <div className="WinPopup">
-        <div className="WinPopup-inner"><>{props.win?<p>Good Job!</p>:<p>the correct answer was {props.answer.toLowerCase()}</p>}<button onClick={()=>window.location.reload()}>Play Again?</button></></div>
-        
+        <div className="WinPopup-inner">
+            <>{props.win?<p>Good Job!</p>:<p>the correct answer was {props.answer.toLowerCase()}</p>}
+            <button onClick={()=>window.location.reload()}>Play Again?</button></>
+            </div>
     </div>
 }
