6b7c8c9a7ee4896040fb6906a1ca4cd773bad95b
Tyler Schulten <tylerschulten@gmail.com>
2023-03-03 20:39:17 -0500

    added reload on win/loss

--- src/Components/WinPopup.tsx
+++ src/Components/WinPopup.tsx
@@ -1,6 +1,7 @@
 import "./WinPopup.css"
 export default function WinPopup(props:{win:boolean, answer:string}){
     return <div className="WinPopup">
-        <div className="WinPopup-inner">{props.win?"Good Job!":`the correct answer was ${props.answer.toLowerCase()}`}</div>
+        <div className="WinPopup-inner"><>{props.win?<p>Good Job!</p>:<p>the correct answer was {props.answer.toLowerCase()}</p>}<button onClick={()=>window.location.reload()}>Play Again?</button></></div>
+        
     </div>
 }
